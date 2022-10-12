c0639d0f5248f43d9ee12991221b10906e391c25
dariodini <dado02rio02@gmail.com>
2022-10-12 22:40:12 +0200

    Javascript file that adds event listener to the buttons

---
+++ Progetto/js/ButtonClass.js
@@ -0,0 +1,39 @@
+class Button{
+
+    number = 0;
+
+    constructor(elem) {
+        this._elem = elem;
+        this.updateNumber(this.number);
+        elem.onclick = this.onClick.bind(this);
+    }
+
+    updateNumber() {
+        document.getElementById('container-number').innerText = this.number;
+    }
+
+    increaseNumber(){
+        this.updateNumber(this.number++);
+    }
+
+    resetNumber(){
+        this.updateNumber(this.number = 0);
+    }
+
+    decreaseNumber(){
+        this.updateNumber(this.number --);
+    }
+
+
+    onClick(event) {
+        let action = event.target.dataset.action;
+        if (action) {
+            this[action]();
+        }
+    };
+}
+
+
+let buttons = document.getElementById('container-buttons');
+
+new Button(buttons);
